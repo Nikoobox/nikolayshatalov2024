208344ef393e694a0989d625451a6415de7aa792
Nikolay Shatalov <nikolayshatalov@Nikolays-MacBook-Pro.local>
2024-07-24 18:41:11 -0400

    b-8: fix scrolling to

--- src/components/Navbar/NavItems.ts
+++ src/components/Navbar/NavItems.ts
@@ -1,5 +1,5 @@
 export const navItems = [
-  { navItemName: "Skills", destination: "skills-destination" },
-  { navItemName: "Projects", destination: "projects-destination" },
-  { navItemName: "Contact", destination: "contact-destination" },
+  { id: "1", navItemName: "Skills", destination: "skills-destination" },
+  { id: "2", navItemName: "Projects", destination: "projects-destination" },
+  { id: "3", navItemName: "Contact", destination: "contact-destination" },
 ];
