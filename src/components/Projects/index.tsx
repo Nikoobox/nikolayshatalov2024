9cce2cbc9eabb6d8b2f595cfeef453d56a86b28b
Nikolay Shatalov <nikolayshatalov@Nikolays-MacBook-Pro.local>
2024-07-27 21:13:02 -0400

    b-11: misc

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,30 +1,65 @@
 import { FC } from "react";
 
-import Box from "@mui/material/Box";
+import { Box, Typography } from "@mui/material";
 
 import { PROJECTS_DATA } from "../Data";
 import PageSection from "../PageSection";
 import Project from "./Project";
 
 const Projects: FC = () => {
-  const projects = PROJECTS_DATA.map((proj) => {
-    return <Project key={proj.id} {...proj} />;
-  });
+  const projects = PROJECTS_DATA.filter((proj) => proj.isMainProject).map(
+    (proj) => {
+      return <Project key={proj.id} {...proj} />;
+    }
+  );
+
+  const otherProjects = PROJECTS_DATA.filter((proj) => !proj.isMainProject).map(
+    (proj) => {
+      return <Project key={proj.id} {...proj} />;
+    }
+  );
 
   return (
-    <PageSection header="Projects" id="projects-destination">
+    <PageSection id="projects-destination">
       <Box
         display="flex"
+        alignItems="center"
         justifyContent="center"
-        flexWrap="wrap"
-        sx={{
-          gap: {
-            xs: 12,
-            sm: 8,
-          },
-        }}
+        flexDirection="column"
       >
-        {projects}
+        <Typography variant="h1" mb={5} mt={20}>
+          Projects
+        </Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          flexWrap="wrap"
+          sx={{
+            gap: {
+              xs: 12,
+              sm: 8,
+            },
+          }}
+        >
+          {projects}
+        </Box>
+
+        <Typography variant="h1" mb={5} mt={20}>
+          Other Projects
+        </Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          flexWrap="wrap"
+          sx={{
+            gap: {
+              xs: 12,
+              sm: 8,
+            },
+          }}
+        >
+          {otherProjects}
+        </Box>
       </Box>
     </PageSection>
   );
