d3396cffe32065657b77a54c6d5578a2da44a6b9
Nikolay Shatalov <nikoobox@gmail.com>
2026-02-01 15:13:59 -0500

    26 resume

--- src/components/Documents/index.ts
+++ src/components/Documents/index.ts
@@ -1,3 +1,4 @@
-import resume from "./Nikolay_Shatalov_July_2024_v3.pdf";
+// import resume from "./Nikolay_Shatalov_July_2024_v3.pdf";
+import resume from "./Nikolay_Shatalov_Resume_2026_v1.pdf";
 
 export { resume };
