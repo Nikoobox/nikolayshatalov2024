394f85f74b68a25175670fd096aa3df898e85fb0
Nikolay Shatalov <nikoobox@gmail.com>
2026-02-01 14:00:49 -0500

    26 add route comps

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-
 import { useTheme } from "@mui/material/styles";
-import { Box } from "@mui/material";
+import { Box, useMediaQuery, Container } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import { PROJECTS_DATA } from "@/components/Data";
+import Page from "@/components/Page";
 
 const ProjectPage = () => {
-  return <Box>Hey there!!!</Box>;
+  const { projectId } = useParams();
+
+  const projectData = PROJECTS_DATA.find(
+    (project) => project.id === +projectId!,
+  );
+
+  console.log("projectData", projectData);
+
+  return (
+    <Page>
+      <Box>ksjdksdj</Box>
+    </Page>
+  );
 };
 
 export default ProjectPage;
