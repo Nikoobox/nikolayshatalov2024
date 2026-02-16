2abb9d597f358c402ebcf684c7c1498188725ced
Nikolay Shatalov <nikoobox@gmail.com>
2026-02-16 09:58:23 -0500

    26 labels

--- src/pages/ProjectPage/InfoLinkRow.tsx
+++ src/pages/ProjectPage/InfoLinkRow.tsx
@@ -17,6 +17,27 @@ const InfoLinkRow: FC<InfoLinkRowProps> = ({ label, link, linkLabel }) => {
   const theme = useTheme();
   const isDarkMode = useDarkTheme();
 
+  const labelUnderlineSx = {
+    fontWeight: LABEL_FONT_WEIGHT,
+    position: "relative",
+    display: "inline-block",
+    zIndex: 1,
+    height: "max-content",
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      left: 0,
+      bottom: 1,
+      width: "75%",
+      height: "10px",
+      background: isDarkMode
+        ? theme.palette.customColors.tealAccent
+        : theme.palette.customColors.classicBlue,
+      opacity: 0.6,
+      zIndex: -1,
+    },
+  };
+
   return (
     <Box
       display="flex"
@@ -31,14 +52,15 @@ const InfoLinkRow: FC<InfoLinkRowProps> = ({ label, link, linkLabel }) => {
         },
       }}
     >
-      <Typography
-        variant="h3"
-        minWidth={LABEL_MIN_WIDTH}
-        color={isDarkMode ? theme.palette.customColors.greyAccent : "inherit"}
-        sx={{ fontWeight: LABEL_FONT_WEIGHT }}
-      >
-        {label}
-      </Typography>
+      <Box minWidth={LABEL_MIN_WIDTH}>
+        <Typography
+          variant="h3"
+          color={isDarkMode ? theme.palette.customColors.greyAccent : "inherit"}
+          sx={labelUnderlineSx}
+        >
+          {label}
+        </Typography>
+      </Box>
       <MyLink
         label={linkLabel}
         link={link}
