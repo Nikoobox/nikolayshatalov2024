ffc58e0a29bd8cb0ef4adacafd83c46124913a3b
Nikolay Shatalov <nikoobox@gmail.com>
2026-05-31 16:03:23 -0400

    31 update skills

--- src/components/Data/projectsInfo.ts
+++ src/components/Data/projectsInfo.ts
@@ -10,8 +10,10 @@ export const PROJECTS_DATA = [
       "TypeScript",
       "Prisma",
       "PostgreSQL",
+      "OAuth",
       "Webhooks",
       "OpenAI API",
+      "Redis",
       "Tailwind",
       "DaisyUI",
       "React Query",
@@ -27,6 +29,7 @@ export const PROJECTS_DATA = [
       "React Hook Form",
       "Sentry",
       "PostHog",
+      "Google Analytics",
     ],
     info: "A multi-user Strava-powered platform that transforms activity data into interactive maps, trends, and AI-driven insights for smarter training. Enriches each activity with weather conditions and location context for deeper performance analysis. Approved by Strava.",
     infoLong:
